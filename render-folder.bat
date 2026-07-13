285cce69ad15c0059bb9bf766d21b07f5fa8be37
gimaxax77-sys <gimaxax77@gmail.com>
2026-07-13 17:19:16 +0000

    render-folder.bat 안내문구 ASCII화 (한글 파싱 깨짐 방지)

--- render-folder.bat
+++ render-folder.bat
@@ -5,10 +5,10 @@ cd /d "%~dp0"
 set "BLENDER=C:\Program Files\Blender Foundation\Blender 4.2\blender.exe"
 set "SPRITE_INPUT=%~dp0input"
 set "SPRITE_OUT=%~dp0out"
-echo input 폴더의 3D 파일을 모두 렌더합니다...
+echo Rendering all 3D files in the input folder...
 echo.
 "%BLENDER%" --background --python "%~dp0scripts\render_folder.py"
 echo.
-echo [OK] 완료! 결과 폴더를 엽니다.
+echo [OK] Done! Opening the out folder.
 if exist "%~dp0out" start "" "%~dp0out"
 pause
